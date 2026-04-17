b14b223047ff77e141c0d24ababeeb4e89d247f6
Igor Muller <igormullersantos@hotmail.com>
2026-04-17 17:38:02 -0300

    Primeiro commit - config inicial do projeto

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <v-main>
+      <Dashboard />
+    </v-main>
+    
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+  import Dashboard from '@/components/Dashboard.vue'
+</script>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container class="fill-height d-flex flex-column justify-center align-center" max-width="1100">
+    Dashboard
+  </v-container>
+</template>
+
+<script setup lang="ts">
+  
+</script>
