9bfc41e5d8f8ea55a289da969ab27f75f46fb80e
Igor Muller <igormullersantos@hotmail.com>
2026-04-18 17:58:42 -0300

    Cria componentes de Card de empresa e Modal de confirmação de deleção de empresa

---
+++ src/components/DeleteCompanyModal.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-dialog
+    :model-value="modelValue"
+    width="auto"
+    @update:model-value="$emit('update:modelValue', $event)"
+  >
+    <v-card max-width="400">
+      <v-card-title class="font-weight-bold">
+        {{ company?.name }}
+      </v-card-title>
+      <v-card-text>
+        Tem certeza de que deseja deletar esta empresa?
+      </v-card-text>
+      <v-card-actions>
+        <v-btn text="Cancelar" @click="$emit('cancel')"></v-btn>
+        <v-btn
+          text="Deletar"
+          color="error"
+          @click="$emit('confirm')"
+          :loading="loading"
+        ></v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup lang="ts">
+import type { Company } from "@/types/companies";
+
+interface Props {
+  modelValue: boolean;
+  company: Company | null;
+  loading: boolean;
+}
+
+const props = defineProps<Props>();
+
+defineEmits<{
+  "update:modelValue": [value: boolean];
+  cancel: [];
+  confirm: [];
+}>();
+</script>
