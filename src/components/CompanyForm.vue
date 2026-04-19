f1de720b6d4fcfc4ba88016dcbc08e2484fe5835
Igor Muller <igormullersantos@hotmail.com>
2026-04-19 00:42:30 -0300

    Realiza melhorias no componente de formulário

--- src/components/CompanyForm.vue
+++ src/components/CompanyForm.vue
@@ -1,12 +1,12 @@
 <template>
   <v-sheet elevation="3" rounded="xl" width="390">
-    <v-form @submit.prevent class="pa-5">
+    <v-form ref="form" @submit.prevent="handleSave" class="pa-5">
       <v-container>
         <v-text-field
           v-model="company.name"
           variant="outlined"
           label="Nome"
-          :rules="[nameRule]"
+          :rules="nameRules"
           :disabled="props.loading"
           class="mb-4"
           required
@@ -16,29 +16,39 @@
           variant="outlined"
           label="CNPJ"
           maxlength="18"
-          :rules="[cnpjRule]"
+          :rules="cnpjRules"
           :disabled="props.loading"
           class="mb-4"
           required
         ></v-text-field>
-        <v-select
-          v-model="activeStatus"
-          variant="outlined"
-          label="Status"
-          :items="['Ativo', 'Inativo']"
-          :disabled="props.loading"
-        >
-        </v-select>
+        <div class="d-flex ga-3 mb-4">
+          <v-chip
+            @click="company.active = true"
+            class="w-50 text-center"
+            color="success"
+            :variant="company?.active ? 'flat' : 'outlined'"
+          >
+            Ativo
+          </v-chip>
+          <v-chip
+            @click="company.active = false"
+            class="w-50 text-center"
+            color="red"
+            :variant="!company?.active ? 'flat' : 'outlined'"
+          >
+            Inativo
+          </v-chip>
+        </div>
 
         <v-btn
-          class="w-100 rounded-xl"
           type="submit"
+          class="w-100 rounded-xl"
           size="large"
           color="success"
           :loading="props.loading"
-          @click="emit('save', company)"
-          >Salvar</v-btn
         >
+          Salvar
+        </v-btn>
       </v-container>
     </v-form>
   </v-sheet>
@@ -46,67 +56,66 @@
 
 <script setup lang="ts">
 import { ref, reactive, watch, computed } from "vue";
+import type { Company } from "@/types/companies";
 import { validateCNPJ } from "@/utils/validateCNPJ";
 import { maskCNPJ } from "@/utils/maskCNPJ";
 import generateId from "@/utils/generateId";
 
 interface Props {
-  modelValue: boolean;
   company: Company | null;
-  loading: boolean;
   mode: "create" | "edit";
+  loading: boolean;
 }
-
 const props = withDefaults(defineProps<Props>(), {
   mode: "create",
 });
-const company = reactive({
+const emit = defineEmits<{
+  (e: "save", company: Company): void;
+}>();
+
+const company = reactive<Company>({
   id: props.company?.id || generateId(),
   name: props.company?.name || "",
   cnpj: props.company?.cnpj || "",
-  active: props.company?.active || true,
-  status: props.status,
+  active: props.company?.active ?? true,
+  employees: props.company?.employees || [],
 });
 
+const form = ref();
+const clearForm = () => {
+  if (props.mode == "create") {
+    form.value.reset();
+    form.value.resetValidation();
+  }
+};
 const cnpjInput = computed({
   get: () => company.cnpj,
   set: (value: string) => {
     company.cnpj = maskCNPJ(value);
   },
 });
-const cnpjRule = (value: string) => validateCNPJ(value) || "CNPJ inválido";
+const cnpjRules = [
+  (v: string) => !!v || "CNPJ é obrigatório",
+  (v: string) => validateCNPJ(v) || "CNPJ inválido",
+];
 const nameRule = (value: string) => !!value?.trim() || "Nome é obrigatório";
 
-const activeStatus = computed({
-  get: () => (company.active ? "Ativo" : "Inativo"),
-  set: (value: string) => {
-    company.active = value === "Ativo";
-  },
-});
+async function handleSave() {
+  const { valid } = await form.value.validate();
+
+  if (!valid) return;
+
+  emit("save", company);
+  clearForm();
+}
 
 watch(
   () => props.company,
   (newCompany) => {
     if (newCompany) {
-      company.id = newCompany.id;
-      company.name = newCompany.name || "";
-      company.cnpj = newCompany.cnpj || "";
-      company.active = newCompany.active;
-      company.status = newCompany.status;
-    } else {
-      company.id = generateId();
-      company.name = "";
-      company.cnpj = "";
-      company.active = true;
-      company.status = props.status;
+      Object.assign(company, newCompany);
     }
   },
   { immediate: true },
 );
-
-const emit = defineEmits<{
-  "update:modelValue": [value: boolean];
-  cancel: [];
-  save: [];
-}>();
 </script>
